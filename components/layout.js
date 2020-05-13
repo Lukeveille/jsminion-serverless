0a3f67b172db12068ca4006eb116bd3eb9a1311e
Lukeveille <luke@lukeleveille.com>
2020-05-12 23:23:52 -0400

    cleanup

--- components/layout.js
+++ components/layout.js
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Building a personal website using Next.js"
         />
         <meta
           property="og:image"
